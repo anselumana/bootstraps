d70613744d60868158baa01ca60257a09a444510
anselumana <anselumana.developer@gmail.com>
2022-11-05 15:27:46 +0100

    now 500 errors return detailed error if env != prod

--- node-rest-api/src/common/error/error.handler.ts
+++ node-rest-api/src/common/error/error.handler.ts
@@ -1,7 +1,8 @@
 import express from "express";
 
 export function errorHandler(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
-  res.status(500).send({ message: "unhandled error occured" });
+  const message = process.env.NODE_ENV === "prod" ? "unhandled error occured" : err.message;
+  res.status(500).json({ message });
 }
 
 export function errorLogger(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
