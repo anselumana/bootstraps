3dc789fd2efcf8d2596b03b67dd255b5c8a8d934
anselumana <anselumana.developer@gmail.com>
2022-11-07 18:38:13 +0100

    removed unused import

--- node-rest-api/src/server.ts
+++ node-rest-api/src/server.ts
@@ -2,7 +2,6 @@ import * as http from 'http';
 import app from './app';
 import config from './common/config/config';
 import logger from './common/logging/logger';
-import dbPromise from "./common/db/db";
 import { exit } from './common/utils/common.utils';
 import db from './common/db/db';
 
