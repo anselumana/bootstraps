e6745911a9440c12ff1d3bb057bd94b6e2310743
anselumana <anselumana.developer@gmail.com>
2022-11-08 11:15:36 +0100

    formatted

--- node-rest-api/src/swagger/swagger.ts
+++ node-rest-api/src/swagger/swagger.ts
@@ -2,13 +2,13 @@ import swaggerAutogen from "swagger-autogen";
 
 
 const doc = {
-   info: {
-     title: "Products API",
-     description: "Simple REST API built with Typescript, Express and MongoDB.",
-   },
-   host: "localhost:8080", // todo: how to make parametric?
-   schemes: ["http"],
- };
+  info: {
+    title: "Products API",
+    description: "Simple REST API built with Typescript, Express and MongoDB.",
+  },
+  host: "localhost:8080", // todo: how to make parametric?
+  schemes: ["http"],
+};
 
 const outputFile = "./src/swagger/static/swagger.json";
 const endpointsFiles = ["./src/app.ts"];
