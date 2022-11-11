21d5a642b8d0cdab6292ae1bfb0262f71d813b74
anselumana <anselumana.developer@gmail.com>
2022-11-11 11:02:08 +0100

    created multiple abstractions for mongodb base repository

--- node-rest-api/src/common/repository/mongodb.repository.ts
+++ node-rest-api/src/common/repository/mongodb.repository.ts
@@ -2,14 +2,64 @@ import { ObjectId, OptionalId, Document } from "mongodb";
 import db from "../db/db";
 import { IReadRepository, IRepository } from "../interfaces/repository.interface";
 
-
-export class MongoDbRepository<T> implements IRepository<T> {
+/**
+ * Abstract class that provides access to the
+ * mongodb collection identified by its name.
+ */
+abstract class MongoDbNamedCollection {
   private collectionName: string;
 
   constructor(collectionName: string) {
     this.collectionName = collectionName;
   }
+  
+  /**
+   * @returns the mongodb collection.
+   */
+  protected collection() {
+    return db.instance().collection(this.collectionName);
+  }
+}
 
+/**
+ * Abstract class that provides base functionality
+ * to mongodb repositories.
+ */
+export abstract class MongoDbBase extends MongoDbNamedCollection {
+  /**
+   * converts each entity from { _id: ObjectId, ... } to { id: string, ... }.
+   * @param entities array of input entities
+   * @returns array of converted entities
+   */
+  protected mapIds(entities: any[]): any[] {
+    return entities.map(e => this.mapId(e));
+  }
+  
+  /**
+   * convert entity from { _id: ObjectId, ... } to { id: string, ... }.
+   * @param entity input entity
+   * @returns converted entity
+   */
+  protected mapId(entity: any): any {
+    if (!entity) {
+      return entity;
+    }
+    const { _id, ...rest } = entity;
+    return {
+      id: _id.toString(), // convert ObjectId to string
+      ...rest,
+    };
+  }
+
+  protected idFilter(id: string) {
+    return { _id: new ObjectId(id) };
+  }
+}
+
+/**
+ * Generic implementation of mongodb repository pattern.
+ */
+export class MongoDbRepository<T> extends MongoDbBase implements IRepository<T> {
   public async list(): Promise<T[]> {
     const entities = await this.collection().find().toArray();
     return this.mapIds(entities) as T[];
@@ -31,31 +81,11 @@ export class MongoDbRepository<T> implements IRepository<T> {
       { $set: entity as Document },
       { returnDocument: "after" });
     const updatedEntity = res.ok === 1 ? res.value : null;
-    return updatedEntity ? this.mapId(updatedEntity) as T : null as T;
+    return this.mapId(updatedEntity) as T;
   };
   
   public async delete(id: string): Promise<boolean> {
     const res = await this.collection().deleteOne(this.idFilter(id));
     return res.deletedCount === 1;
   };
-
-  private mapIds(entities: any[]): any[] {
-    return entities.map(e => this.mapId(e));
-  }
-
-  private mapId(entity: any): any {
-    const { _id, ...rest } = entity;
-    return {
-      id: _id.toString(), // convert ObjectId to string
-      ...rest,
-    };
-  }
-
-  private idFilter(id: string) {
-    return { _id: new ObjectId(id) };
-  }
-
-  private collection() {
-    return db.instance().collection(this.collectionName);
-  }
 }
