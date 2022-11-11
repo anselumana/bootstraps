397ec79ee8b6a591e0b1bc3677d21ed6d7fd7b33
anselumana <anselumana.developer@gmail.com>
2022-11-11 10:07:43 +0100

    create IReadRepository<T> and IWriteRepository<T>

--- node-rest-api/src/common/interfaces/repository.interface.ts
+++ node-rest-api/src/common/interfaces/repository.interface.ts
@@ -1,9 +1,14 @@
 
 
-export interface IRepository<T> {
+export interface IReadRepository<T> {
   list: () => Promise<T[]>;
   get: (id: string) => Promise<T | null>;
+}
+
+export interface IWriteRepository<T> {
   create: (entity: T) => Promise<string>;
   update: (id: string, entity: T) => Promise<T | null>;
   delete: (id: string) => Promise<boolean>;
 }
+
+export interface IRepository<T> extends IReadRepository<T>, IWriteRepository<T> {}
