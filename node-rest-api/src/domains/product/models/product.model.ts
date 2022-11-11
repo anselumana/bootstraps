7c2c42f32f9e576602fb4ec540b754c239f09584
anselumana <anselumana.developer@gmail.com>
2022-11-11 10:38:22 +0100

    update min name length on product model

--- node-rest-api/src/domains/product/models/product.model.ts
+++ node-rest-api/src/domains/product/models/product.model.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 // base model
 export const productZod = z.object({
   id: z.string(),
-  name: z.string().min(6),
+  name: z.string().min(1),
   price: z.number().min(0),
   inStock: z.boolean(),
 }).strict();
